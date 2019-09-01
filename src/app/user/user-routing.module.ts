6ef68ccf08074af95e713e395fe9fde98383e6d5
Ricardo Reais <Ricardo.Reais@vortal.biz>
2019-09-01 22:41:04 +0100

    Removed app profile from game layout

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
-import { ProfileComponent } from '../shared/components/profile/profile.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuard } from '../shared/guards/auth.guard';
 
 const routes: Routes = [
