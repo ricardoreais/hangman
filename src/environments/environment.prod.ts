d40378b79fff6b2b3c2164718a7b280db1e66e6a
Ricardo Reais <Ricardo.Reais@vortal.biz>
2019-09-02 01:23:17 +0100

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  maxIncorrectGuessCount: 5,
-  words: ['3dhubs', 'marvin', 'print', 'filament', 'order', 'layer'],
-  defaultLanguage: 'en',
-  emptyChar: '_'
-};
