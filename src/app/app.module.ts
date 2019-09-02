935935dfe18990f66b1d82c179defe6898e896ee
Ricardo Reais <Ricardo.Reais@vortal.biz>
2019-09-02 01:35:11 +0100

    Fixed translations missing after deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
